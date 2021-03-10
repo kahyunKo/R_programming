2d0dd0264958e637c798e7156671058997ca632e
kahyunKo <78471342+kahyunKo@users.noreply.github.com>
2021-03-10 15:19:12 +0900

    Add files via upload

---
+++ sec04_01.R
@@ -0,0 +1,10 @@
+english <- c(90, 80, 60, 70)
+english
+math <- c(50, 60, 100, 20)
+math
+df_midterm <- data.frame(english, math)
+df_midterm
+class <- c(1,1,2,2)
+class
+df_midterm <-data.frame(english, math, class)
+df_midterm
